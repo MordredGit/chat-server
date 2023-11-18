f99301c1a34353037f5dd04aac5b3ea98f2e6e55
MordredGit <mshagun2001@gmail.com>
2023-11-18 19:43:50 +0530

    Feat: Add getUsers controller

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { updateUserProfile } from "../controllers/user";
+import { getUsers, updateUserProfile } from "../controllers/user";
 import { protect } from "../controllers/auth";
 
 const userRouter = Router();
 
 userRouter.patch("/update-user-profile", protect, updateUserProfile);
+userRouter.get("/get-users", protect, getUsers);
 
 export default userRouter;
